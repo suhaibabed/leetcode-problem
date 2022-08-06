7232f62a3daf846b807b3dcc649b9397bc4f104b
sohiab <you@example.com>
2022-08-06 23:10:34 +0300

    leetcode problem solve

---
+++ RunningSumof1dArray.js
@@ -0,0 +1,9 @@
+var runningSum = function (nums) {
+  let total = 0;
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    total = total + nums[i];
+    result.push(total);
+  }
+  return result;
+};
