fd1192b684c30e36a1000814927d635be0533750
sohiab <you@example.com>
2022-08-13 15:31:36 +0300

    leetcode solve problem in array

---
+++ Find Numbers withEvenNumberofDigits.js
@@ -0,0 +1,25 @@
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
+// Example 1:
+
+// Input: nums = [12,345,2,6,7896]
+// Output: 2
+// Explanation:
+// 12 contains 2 digits (even number of digits).
+// 345 contains 3 digits (odd number of digits).
+// 2 contains 1 digit (odd number of digits).
+// 6 contains 1 digit (odd number of digits).
+// 7896 contains 4 digits (even number of digits).
+// Therefore only 12 and 7896 contain an even number of digits.
+
+var findNumbers = function (nums) {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let num = String(nums[i]);
+
+    if (num.length % 2 === 0) result++;
+  }
+
+  return result;
+};
